b2e087a5f280f7564fc2170c4577086409466ed4
Mannu6006 <Mannu6006@users.noreply.github.com>
2025-11-19 11:24:10 +0530

    bfghfsf

--- server.js
+++ server.js
@@ -6,8 +6,15 @@ import orderRoutes from "./routes/order.routes.js";
 const app = express();
 app.use(express.json());
 
+// Default root route
+app.get("/", (req, res) => {
+  res.send("Backend server running successfully 🚀");
+});
+
+// Routes
 app.use("/auth", authRoutes);
 app.use("/catalog", catalogRoutes);
 app.use("/order", orderRoutes);
 
+// Server
 app.listen(5000, () => console.log("Server running on port 5000"));
